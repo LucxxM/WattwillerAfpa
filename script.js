95ebc4dcf531a39a65724415b9fc5ebe857e37a2
Luc <luchio83@hotmail.fr>
2022-06-05 19:42:06 +0200

    add JS and one MediaQuery

--- script.js
+++ script.js
@@ -1,2 +1,98 @@
+const items = document.querySelectorAll('.item');
+const arrowsLeft = document.querySelectorAll('.fa-arrow-left');
+const arrowsRight = document.querySelectorAll('.fa-arrow-right');   
+const active = document.querySelector('.active');
+
+console.log(items);
+console.log(arrowsLeft);
+console.log(arrowsRight);
+console.log(active);
+
+
+function onclickLeft() {
+    const active = document.querySelector('.active');
+    const prev = active.previousElementSibling;
+    active.classList.remove('active');
+    prev.classList.add('active');
+}
+
+
+function onclickRight() {
+    const active = document.querySelector('.active');
+    const next = active.nextElementSibling;
+    active.classList.remove('active');
+    next.classList.add('active');
+}
+
+
+function onhoverLeft() {
+    arrowsLeft.forEach(arrow => {
+        if (arrow.classList.contains('grey-arrow')){
+            arrow.classList.remove('fa-arrow-left-long');
+            arrow.classList.add('fa-arrow-left');
+        }else{
+
+            arrow.classList.add('fa-arrow-left-long');
+            arrow.classList.remove('fa-arrow-left');
+        }
+    }
+    )
+}
+function onhoverLeft2() {
+    arrowsLeft.forEach(arrow => {
+        
+        arrow.classList.remove('fa-arrow-left-long');
+        arrow.classList.add('fa-arrow-left');
+    }
+    )
+}
+
+
+function onhoverRight() {
+    arrowsRight.forEach(arrow => {
+        if (arrow.classList.contains('grey-arrow')){
+        arrow.classList.remove('fa-arrow-right-long');
+        arrow.classList.add('fa-arrow-right');
+        }else{
+            arrow.classList.add('fa-arrow-right-long');
+            arrow.classList.remove('fa-arrow-right');
+        }
+
+    }
+    )
+}
+function onhoverRight2() {
+    arrowsRight.forEach(arrow => {
+        arrow.classList.remove('fa-arrow-right-long');
+        arrow.classList.add('fa-arrow-right');
+    }
+    )
+}
+
+
+arrowsLeft.forEach(arrow => {
+    arrow.addEventListener('mouseenter', onhoverLeft);
+}
+);
+arrowsLeft.forEach(arrow => {
+    arrow.addEventListener('mouseleave', onhoverLeft2);
+}
+);
+
+
+arrowsRight.forEach(arrow => {
+    arrow.addEventListener('mouseenter', onhoverRight);
+}
+);
+arrowsRight.forEach(arrow => {
+    arrow.addEventListener('mouseleave', onhoverRight2);
+}
+);
+
+
+
+
+
+
 
 
